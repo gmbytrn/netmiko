3059f90db0ae94326dc895b1d07e50b7ec05ea72
gmbytrn <gmbytrn@users.noreply.github.com>
2015-07-02 22:13:25 +1200

    Update alcatel_7210_ssh.py

--- netmiko/alcatel/alcatel_7210_ssh.py
+++ netmiko/alcatel/alcatel_7210_ssh.py
@@ -2,4 +2,15 @@ from netmiko.ssh_connection import SSHConnection
 
 class Alcatel7210SSH(SSHConnection):
 
-    pass
+    def enable(self):
+        '''
+        No enable mode on Alcatel 7210
+        '''
+        pass
+
+
+    def exit_enable_mode(self, exit_command=''):
+        '''
+        No enable mode on Alcatel 7210
+        '''
+        pass
